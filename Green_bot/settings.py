8783a68e831553017b7b52dee38bb4f6efc89f8c
Mikle <m.n.demianenko@gmail.com>
2018-08-07 17:54:01 +0300

    He will talk with app

--- Green_bot/settings.py
+++ Green_bot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'green_bot_app.apps.GreenBotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
