f72fac4899a82d2b52c88ff786ce42fa0ecbafda
Mikle <m.n.demianenko@gmail.com>
2018-08-22 14:58:02 +0300

    Рабочий бот.(Изм.1-7) В settings название бд прежнее

--- Green_bot/settings.py
+++ Green_bot/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Green_bot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Water_Bot_db',
+        'NAME': 'Bot_GreenTown_db',
         'USER': 'GreeBot_db_user',
         'PASSWORD': 'pass',
         'HOST': '127.0.0.1', # set in docker-compose.yml
