438f972571396fb468b03b503659c8600c2a2431
Mikle <m.n.demianenko@gmail.com>
2018-08-27 17:05:47 +0300

    меняем покупателя одним кликом )

--- green_bot_app/admin.py
+++ green_bot_app/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-
 from .models import Organisation, TelegramUser
+
+
+def change_buyer(modeladmin, request, queryset):
+    Organisation.objects.filter(buyer=True).update(buyer=False)
+    queryset.update(buyer=True)
+
+
+change_buyer.short_description = 'Изменить покупателя'
 
 
 class OrganisationAdmin(admin.ModelAdmin):
     list_display = ('name', 'buyer', 'phone_number')
+    actions = [change_buyer, ]
 
 
 class TelegramUserAdmin(admin.ModelAdmin):
