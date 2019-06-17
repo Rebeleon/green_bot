46be38dcf09e3ea408b8599819d626b097996324
FerumFlex <ferumflex@gmail.com>
2019-06-17 11:28:55 +0300

    fxied

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.7
 
 import asyncio
 import websockets
@@ -7,69 +7,63 @@ import logging
 
 from aiohttp import web
 
+WEBSOCKET_HOST = 'localhost'
+WEBSOCKET_PORT = 9000
+HTTP_HOST = 'localhost'
+HTTP_PORT = 8080
+AUTH_TOKEN = 'Rg4P1gQeMy'
 
-import coloredlogs
-coloredlogs.install(level='INFO')
-
-
+logging.basicConfig(level=logging.INFO)
 queue_open_door = asyncio.Queue(maxsize=1)
-
-TOKEN = 'Rg4P1gQeMy'
 
 
 async def serve_client(websocket, path):
-    logging.info(f'Connected {websocket}')
+    logging.info('Websocket client connected')
 
     # authentication
     try:
-        auth = await asyncio.wait_for(websocket.recv(), timeout=20)
-        if auth != TOKEN:
-            logging.info(f'Bad auth {auth}')
+        auth_token = await asyncio.wait_for(websocket.recv(), timeout=20)
+        if auth_token != AUTH_TOKEN:
+            logging.info(f'Invalid auth token')
             return
+        else:
+            logging.info('Successfully authorized')
+            await asyncio.wait_for(websocket.send('authorized'), timeout=0.5)
     except asyncio.TimeoutError:
         return
 
-    latest_ping = None
-    latest_pong = None
     while True:
-        if not latest_ping or latest_ping < datetime.datetime.utcnow() - datetime.timedelta(seconds=20):
-            latest_ping = datetime.datetime.utcnow()
-            logging.info('Send ping')
-            await websocket.send('ping')
-        
         try:
             action = await asyncio.wait_for(websocket.recv(), timeout=0.5)
-            if action == 'pong':
-                latest_pong = datetime.datetime.utcnow()
-                logging.info('Received pong')
-            elif action == 'opened':
+            if action == 'opened':
                 logging.info('Received opened')
         except asyncio.TimeoutError:
             pass
+        except websockets.exceptions.ConnectionClosed:
+            logging.info('Websocket client disconnected')
+            return
 
-        # do not try to open door, if connection is idle
-        threshold = datetime.datetime.utcnow() - datetime.timedelta(minutes=2)
-        if latest_pong and latest_ping and latest_pong > threshold:
-            try:
-                data = await asyncio.wait_for(queue_open_door.get(), timeout=0.5)
-                if data and data['action'] == 'door' and data['expired'] > datetime.datetime.utcnow():
-                    logging.info('Send open door to device')
-                    await websocket.send('door')
-            except asyncio.TimeoutError:
-                pass
-            
+        try:
+            data = await asyncio.wait_for(queue_open_door.get(), timeout=0.5)
+            if data and data['action'] == 'door' and data['expired'] > datetime.datetime.utcnow():
+                # ODOR! - https://knowyourmeme.com/memes/hold-the-door
+                logging.info('Sent open door command to device')
+                await asyncio.wait_for(websocket.send('ODOR!'), timeout=0.5)
+        except asyncio.TimeoutError:
+            pass
 
-start_server = websockets.serve(serve_client, '144.76.100.208', 9000)
-asyncio.get_event_loop().run_until_complete(start_server)
+logging.info(f'Started websockets server at ws://{WEBSOCKET_HOST}:{WEBSOCKET_PORT}')
+
+asyncio.get_event_loop().run_until_complete(websockets.serve(serve_client, WEBSOCKET_HOST, WEBSOCKET_PORT))
 
 
 async def handle(request):
-    if request.headers.get('Authorization') == TOKEN:
+    if request.headers.get('Authorization') == AUTH_TOKEN:
         logging.info('Request to open door')
 
         if not queue_open_door.empty():
             await queue_open_door.get()
-    
+
         data = {
             'action': 'door',
             'expired': datetime.datetime.utcnow() + datetime.timedelta(seconds=10),
@@ -77,13 +71,15 @@ async def handle(request):
         await queue_open_door.put(data)
         return web.json_response({'status': 'ok'})
     else:
-        logging.info('Error request to open door. No token')
+        logging.info('Open door request error: No auth token')
 
         return web.json_response({'status': 'error'})
-
 
 app = web.Application()
 app.add_routes([
     web.post('/', handle),
 ])
-web.run_app(app, port=8080)
+
+logging.info(f'Started HTTP server at http://{HTTP_HOST}:{HTTP_PORT}')
+
+web.run_app(app, port=HTTP_PORT)
